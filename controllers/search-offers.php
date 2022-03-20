c5dfb5fd64f847646251347fd827d3b4c97fb343
foasth <ankhoud@yahoo.fr>
2022-03-20 16:30:34 +0100

    update search

--- controllers/search-offers.php
+++ controllers/search-offers.php
@@ -4,21 +4,22 @@ require '../models/model.php';
 
 $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 $limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 10;
+$search = isset($_GET['search']) ? $_GET['search'] : '';
 $offset = ($page - 1) * $limit;
 $params = array(
     ':limit' => array($limit, PDO::PARAM_INT),
     ':offset' => array($offset, PDO::PARAM_INT),
+    ':search' => array($search, PDO::PARAM_STR_CHAR)
 );
 
 $bdd = new DB();
 
-$results = $bdd->select('select * from company order by id_com LIMIT :offset, :limit;', $params);
+$results = $bdd->select('select * from offer  WHERE INSTR(name_offer, :search) > 0 order by id_offer LIMIT :offset, :limit;', $params);
 
 $resultEncode = array();
 foreach ($results as $key => $value) {
     # code...
     $resultEncode[$key] = $results[$key];
-    $resultEncode[$key]['logo_com'] = base64_encode($results[$key]['logo_com']);
 }
 
 $echo = array(
--- controllers/search-users.php
+++ controllers/search-users.php
@@ -4,21 +4,23 @@ require '../models/model.php';
 
 $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 $limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 10;
+$search = isset($_GET['search']) ? $_GET['search'] : '';
 $offset = ($page - 1) * $limit;
 $params = array(
     ':limit' => array($limit, PDO::PARAM_INT),
     ':offset' => array($offset, PDO::PARAM_INT),
+    ':search' => array($search, PDO::PARAM_STR_CHAR)
 );
 
 $bdd = new DB();
 
-$results = $bdd->select('select * from user order by id_user LIMIT :offset, :limit;', $params);
+$results = $bdd->select('select * from user  WHERE INSTR(firstname, :search) > 0 order by id_user LIMIT :offset, :limit;', $params);
 
 $resultEncode = array();
 foreach ($results as $key => $value) {
     # code...
     $resultEncode[$key] = $results[$key];
-   
+  
 }
 
 $echo = array(
