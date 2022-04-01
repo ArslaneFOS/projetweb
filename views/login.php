0999bec2e59c8fd9a659d39e46ba251edcf3f1a1
foasth <ankhoud@yahoo.fr>
2022-04-01 20:25:04 +0100

    1706

--- views/login.php
+++ views/login.php
@@ -209,5 +209,11 @@
             </div>
     </div>
 </footer>
+<div class="input-group rounded">
+  <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+  <span class="input-group-text border-0" id="search-addon">
+    <i class="fas fa-search"></i>
+  </span>
+</div>
     </body>
 </html>
---
+++ views/offers.php
@@ -0,0 +1,99 @@
+!<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/views/assets/css/offers.css" rel="stylesheet">
+        <link href="/views/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/views/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="/views/assets/vendor/fontawesome/css/fontawesome.min.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="/views/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  </head>
+  <body>
+  <header id="header" class="fixed-top d-flex align-items-center">
+            <div class="container d-flex align-items-center justify-content-between">
+        
+              <div class="logo">
+                <img  class="logo" src="/views/assets/img/neo-neo-logo_1.svg">
+              </div>
+        
+              <nav id="navbar" class="navbar">
+                <ul>
+                  <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                  <li><a class="nav-link scrollto" href="#offers">Offers</a></li>
+                  <li><a class="nav-link scrollto" href="#companies">Companies</a></li>
+                  <li><a class="nav-link scrollto" href="#portfolio">Interns</a></li>
+                  <li><a class="nav-link scrollto" href="#portfolio">|</a></li>
+                  <!--<li><a class="nav-link scrollto" href="#team">Login</a></li>
+                  <li class="dropdown"><a href="#"><span>Random DropDown</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                      <li><a href="#">Drop Down 1</a></li>
+                      <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                        <ul>
+                          <li><a href="#">Deep Drop Down 1</a></li>
+                          <li><a href="#">Deep Drop Down 2</a></li>
+                          <li><a href="#">Deep Drop Down 3</a></li>
+                          <li><a href="#">Deep Drop Down 4</a></li>
+                          <li><a href="#">Deep Drop Down 5</a></li>
+                        </ul>
+                      </li>
+                      <li><a href="#">Drop Down 2</a></li>
+                      <li><a href="#">Drop Down 3</a></li>
+                      <li><a href="#">Drop Down 4</a></li>
+                    </ul>
+                  </li>
+                  <li><a class="nav-link scrollto" href="#contact">Contact</a></li> -->
+                  <li><a class="nav-link scrollto" href="./login.html">Login</a></li>
+                </ul>
+                
+              </nav><!-- .navbar -->
+        
+            </div>
+          </header><!-- End Header -->
+
+    
+    <div class="searchbar">
+        
+            <div class="container">
+                <div class="row height d-flex  align-items-center">
+                    <div class="col-md-6">
+                    <div class="form-outline">
+                         <input type="search" id="form1" class="form-control" placeholder="Type query" aria-label="Search" />
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="search-select">
+                <div class="container">
+                    <div class="row height d-flex justify-content-center align-items-center">
+                    <div class="col-md-3">
+            <select class="form-select" aria-label="Default select example">
+                
+                <option selected>Select a Location</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        
+    </div>
+    <div class="searchbar2">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+
+                </div>
+            </div>
+        </div>
+    </div>
+      
+
+  
+  </body>
