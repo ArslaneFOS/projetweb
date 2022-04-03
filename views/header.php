97645950177979f66c4ccb16d3617bc68a52448c
ArslaneFOS <arslane02@gmail.com>
2022-04-03 11:43:53 +0100

    update

--- views/header.php
+++ views/header.php
@@ -7,10 +7,10 @@
 
       <nav id="navbar" class="navbar navbar-light">
         <ul>
-          <li><a class="nav-link scrollto" href="home">Home</a></li>
-          <li><a class="nav-link scrollto" href="offers">Offers</a></li>
-          <li><a class="nav-link scrollto" href="companies">Companies</a></li>
-          <li><a class="nav-link scrollto" href="users">Interns</a></li>
+          <li><a class="nav-link scrollto" href="/home">Home</a></li>
+          <li><a class="nav-link scrollto" href="/offers">Offers</a></li>
+          <li><a class="nav-link scrollto" href="/companies">Companies</a></li>
+          <li><a class="nav-link scrollto" href="/users">Interns</a></li>
           <li><a class="nav-link scrollto">|</a></li>
           <?php
           if (isset($_SESSION['id_user'])) {
@@ -24,12 +24,12 @@
             <?php
             if ($_SESSION['user-type'] == 'admin' || $_SESSION['user-type'] == 'pilot' || $_SESSION['user-type'] == 'pilot') {
             ?>
-            <li><span class="dropdown-item"><a style="color: black;" href="admin/companies">Companies</a></span></li>
-            <li><span class="dropdown-item"><a style="color: black;" href="admin/offers">Offers</a></span></li>
-            <li><span class="dropdown-item"><a style="color: black;" href="admin/students">Students</a></span></li>
-            <li><span class="dropdown-item"><a style="color: black;" href="admin/pilots">Pilots</a></span></li>
-            <li><span class="dropdown-item"><a style="color: black;" href="admin/representatives">Representatives</a></span></li>
-            <li><span class="dropdown-item"><a style="color: black;" href="admin/applications">Applications</a></span></li>
+            <li><span class="dropdown-item"><a style="color: black;" href="/admin/companies">Companies</a></span></li>
+            <li><span class="dropdown-item"><a style="color: black;" href="/admin/offers">Offers</a></span></li>
+            <li><span class="dropdown-item"><a style="color: black;" href="/admin/students">Students</a></span></li>
+            <li><span class="dropdown-item"><a style="color: black;" href="/admin/pilots">Pilots</a></span></li>
+            <li><span class="dropdown-item"><a style="color: black;" href="/admin/representatives">Representatives</a></span></li>
+            <li><span class="dropdown-item"><a style="color: black;" href="/admin/applications">Applications</a></span></li>
 
             <?php
             } else {
