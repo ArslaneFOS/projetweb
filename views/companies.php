bffc85cee0bd93e1b833bead770b65c133e4ac1e
foasth <ankhoud@yahoo.fr>
2022-04-03 09:38:47 +0100

    haha

--- views/companies.php
+++ views/companies.php
@@ -14,7 +14,6 @@ if (!(has_student_access_level() || has_admin_access_level() || has_pilot_access
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Companies</title>
-    <link href="/views/assets/css/home.css" rel="stylesheet">
     <link href="/views/assets/css/companies.css" rel="stylesheet">
     <link href="/views/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="/views/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
