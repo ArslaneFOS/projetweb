a7c2792621263f722da195434d7da60d8aa34a69
foasth <ankhoud@yahoo.fr>
2022-04-03 10:31:33 +0100

    update navbar

--- views/admin-students.php
+++ views/admin-students.php
@@ -19,13 +19,13 @@ if (!(has_admin_access_level() || has_pilot_access_level() || (has_representativ
 <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
 <link href="/views/assets/img/apple-touch-icon.png" rel="apple-touch-icon"> 
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
-  <link href="/views/assets/vendor/aos/aos.css" rel="stylesheet">
+  
   <link href="/views/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="/views/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="/views/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="/views/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="/views/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-  <link href="/views/assets/css/style.css" rel="stylesheet">
+  <link href="/views/assets/css/home.css" rel="stylesheet">
   <script src="/views/assets/vendor/aos/aos.js"></script>
   <script src="/views/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="/views/assets/vendor/glightbox/js/glightbox.min.js"></script>
